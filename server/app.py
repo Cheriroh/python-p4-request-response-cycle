3aa5f9d788041f1675e20e27eb71b18b746bbc31
Cheriroh <prudencec2019@gmail.com>
2025-04-05 19:56:43 +0300

    test passed

--- server/app.py
+++ server/app.py
@@ -2,7 +2,37 @@
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(_name_)
 
-if __name__ == '__main__':
+@app.route('/')
+def index():
+    return "<h1>Python Operations with Flask Routing and Views</h1>"
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)  
+    return param  
+
+@app.route('/count/<int:param>')
+def count(param):
+    return "\n".join(str(i) for i in range(param)) + "\n"
+
+@app.route('/math/<int:num1>/<string:operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        result = num1 / num2 if num2 != 0 else "Cannot divide by zero"
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return "Invalid operation"
+    
+    return str(result)
+
+if _name_ == '_main_':
     app.run(port=5555, debug=True)
